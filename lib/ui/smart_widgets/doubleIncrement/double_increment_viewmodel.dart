2ba5e9d0c4275d91202de1244adebf5f56af5b73
Circaadia <circaadia@Circaadias-MacBook-Pro.local>
2021-09-29 16:30:24 +0500

    Reactive Services And ViewModels

--- lib/ui/smart_widgets/doubleIncrement/double_increment_viewmodel.dart
+++ lib/ui/smart_widgets/doubleIncrement/double_increment_viewmodel.dart
@@ -2,7 +2,7 @@ import 'package:reactive_services/app/locator.dart';
 import 'package:reactive_services/services/counter_service.dart';
 import 'package:stacked/stacked.dart';
 
-class DoubleIncrementViewModel extends BaseViewModel{
+class DoubleIncrementViewModel extends ReactiveViewModel{
   final counterSer = locator<CounterServices>();
   int get counterr => counterSer.count;
 
@@ -10,4 +10,7 @@ class DoubleIncrementViewModel extends BaseViewModel{
       counterSer.doubleCounter();
       notifyListeners();
     }
+
+  @override
+  List<ReactiveServiceMixin> get reactiveServices => [counterSer];
 }
--- lib/ui/smart_widgets/singleIncrement/single_increment_viewmodel.dart
+++ lib/ui/smart_widgets/singleIncrement/single_increment_viewmodel.dart
@@ -2,13 +2,16 @@ import 'package:reactive_services/app/locator.dart';
 import 'package:reactive_services/services/counter_service.dart';
 import 'package:stacked/stacked.dart';
 
-class SingleIncrementViewModel extends BaseViewModel{
+class SingleIncrementViewModel extends ReactiveViewModel{
     final counterSer = locator<CounterServices>();
     int get counterr => counterSer.count;
-    
+
     void updateCounter(){
       counterSer.incrementCounter();
       notifyListeners();
     }
+
+  @override
+  List<ReactiveServiceMixin> get reactiveServices => [counterSer];
 
 }
